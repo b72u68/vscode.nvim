7bd09a61a629d4845b8c90f845465751d289e552
b72u68 <mdinh@hawk.iit.edu>
2022-10-26 11:33:16 -0500

    Refactored vscode module

--- lua/vscode/init.lua
+++ lua/vscode/init.lua
@@ -1,7 +1,32 @@
 local vscode = require("vscode.vscode")
 
-function stop_using_neovim()
-    print(vscode.get_message())
+local message = [[ 
+                                            &GYJJ5B&
+                                         &GYJYJ777?JP
+                              &        #PYYYYYJ777777
+                            GYJ5G&   BPYYYYYYPG??????
+                            G5JJJYPG5YYYYYPB& #??????
+                              #PYYJJYYY5B&    #??????
+                              #G5YYYYYY5G&    #??????
+                            BP5555GG5YYYYYPB& #??????
+                            BP5PB&   BPYYYYY5GG??????
+                              &        #G5YYY5YJJJ???
+                                         &G5555J?JJYP
+                                           &BP5YPB&
+ 
+__      ___                 _    _____ _             _ _          _____          _
+\ \    / (_)               | |  / ____| |           | (_)        / ____|        | |
+ \ \  / / _ ___ _   _  __ _| | | (___ | |_ _   _  __| |_  ___   | |     ___   __| | ___
+  \ \/ / | / __| | | |/ _` | |  \___ \| __| | | |/ _` | |/ _ \  | |    / _ \ / _` |/ _ \
+   \  /  | \__ \ |_| | (_| | |  ____) | |_| |_| | (_| | | (_) | | |___| (_) | (_| |  __/
+    \/   |_|___/\__,_|\__,_|_| |_____/ \__|\__,_|\__,_|_|\___/   \_____\___/ \__,_|\___|
+ 
+ 
+            Stop using NeoVim. Use VsCode like a real programmer instead.
+]]
+
+local function stop_using_neovim()
+    print(message)
     if (vscode.is_vscode_installed()) then
         vscode.open_vscode()
     else
@@ -12,11 +37,11 @@ function stop_using_neovim()
             vscode.open_vscode()
         else
             print("Cannot download VSCode at the moment.")
-            print(vscode.get_download_message())
+            print("Download VSCode at: https://code.visualstudio.com/download")
         end
     end
     -- Waiting for user input to exit VSCode message screen
-    vim.ui.input({ prompt = "Press enter to continue..."},
+    vim.ui.input({ prompt = "\nPress enter to continue..."},
         function (_)
             vim.cmd("qa!")
         end)
